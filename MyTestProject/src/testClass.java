a27934f4293bf790a5b92f6aaf5caf4a15c3e635
kris <kbsys.2009@gmail.com>
2017-01-28 18:45:24 -0500

    sample test class

---
+++ MyTestProject/src/testClass.java
@@ -0,0 +1,15 @@
+
+public class testClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		testMethod();
+	}
+	
+	
+	public static void testMethod() {
+		System.out.println("name test method");
+		
+	}
+
+}
