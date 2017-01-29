74c826953a0af4b7daaff8b9862ae720d310a9ae
kris <kbsys.2009@gmail.com>
2017-01-28 19:13:14 -0500

    Added another sysout

--- MyTestProject/src/testClass.java
+++ MyTestProject/src/testClass.java
@@ -10,6 +10,7 @@ public class testClass {
 	public static void testMethod() {
 		System.out.println("name test method");
 		System.out.println("added change");
+		System.out.println("added another change");
 		
 	}
 
