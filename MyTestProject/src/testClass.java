baa3d8cec197941f733209e0d0c2ac1d3be1e2a7
kbsys2017 <kbsys.2009@gmail.com>
2021-11-13 21:53:08 -0500

    Update testClass.java

--- MyTestProject/src/testClass.java
+++ MyTestProject/src/testClass.java
@@ -4,18 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 public class testClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Sample Test");
+	}
 
-	@Test
-    public void OpenBrowser()  {
-        WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-        options.addArguments("disable-gpu");
-        driver = new ChromeDriver(options);
-        driver.get("https://www.google.com");
-        System.out.println("Title of the page is: " + driver.getTitle());
-        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
-    }
-
+	
 }
