c9cc3589ed223764beb90f46985975304b481b1e
kbsys2017 <kbsys.2009@gmail.com>
2021-11-13 22:19:46 -0500

    Test Changes

--- MyTestProject/src/testClass.java
+++ MyTestProject/src/testClass.java
@@ -1,18 +1,19 @@
 import org.junit.Assert;
 import org.junit.Test;
-
-
-public class FirstTest {
-
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+public class testClass {
+        
     @Test
-    public void OpenBrowser()  {
-        
-        System.out.println("Test Start");
-        
-        
-         System.out.println("Test Finished");
-    }
+		public static void testMethod() {
+		System.out.println("name test method");
+		System.out.println("added change");
+		System.out.println("added another change");
+		System.out.println("added test change");
 
-
+		//Added comment
+		System.out.println("Git Repository");
+	}
+	
 }
