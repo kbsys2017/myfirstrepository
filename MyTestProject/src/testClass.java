1945ce5c2c2fc524ca3a7a551ae2cf3e342d668d
kbsys2017 <kbsys.2009@gmail.com>
2021-11-13 22:11:18 -0500

    Update testClass.java

--- MyTestProject/src/testClass.java
+++ MyTestProject/src/testClass.java
@@ -1,8 +1,6 @@
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
 
 public class FirstTest {
 
